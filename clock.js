8e31457567eb9733b42cb47530cd73e86d320cab
Danilo Barros <danilobjr@gmail.com>
2018-06-13 16:44:29 -0300

    responsive rendering

--- clock.js
+++ clock.js
@@ -4,14 +4,10 @@ const termSize = require('term-size');
 const readline = require('readline');
 const chalk = require('chalk');
 
-const constants = {
-  asd: 1
-};
-
 // const CURRENT_THEME_NAME = 'default'; // TODO: Configurable from env.
 
-const themeConfig = require(`./config.js`)({ constants, chalk });
-const themeTypeface = require(`./typeface.js`)({ constants });
+// const themeConfig = require(`./config.js`)({ chalk });
+const defaultTheme = require(`./typeface.js`);
 
 let lastOutputLength = 0;
 let output = [];
@@ -45,23 +41,23 @@ function printEmptyLines(count = 0) {
   }
 }
 
-function buildClock(time) {
+function buildClock(time, theme) {
   const hours = time.substr(0, 2);
   const minutes = time.substr(3, 2);
   const seconds = time.substr(6, 2);
 
   const numberMatrix = [
-    themeTypeface.numbers[hours[0]],
-    themeTypeface.numberSpace,
-    themeTypeface.numbers[hours[1]],
-    themeTypeface.numberSeperator,
-    themeTypeface.numbers[minutes[0]],
-    themeTypeface.numberSpace,
-    themeTypeface.numbers[minutes[1]],
-    themeTypeface.numberSeperator,
-    themeTypeface.numbers[seconds[0]],
-    themeTypeface.numberSpace,
-    themeTypeface.numbers[seconds[1]]
+    theme.numbers[hours[0]],
+    theme.numberSpace,
+    theme.numbers[hours[1]],
+    theme.numberSeperator,
+    theme.numbers[minutes[0]],
+    theme.numberSpace,
+    theme.numbers[minutes[1]],
+    theme.numberSeperator,
+    theme.numbers[seconds[0]],
+    theme.numberSpace,
+    theme.numbers[seconds[1]]
   ];
 
   const stringLines = [];
@@ -75,24 +71,23 @@ function buildClock(time) {
   });
 
   // color clock lines
-  return stringLines.map(line => themeConfig.clock.styleNumber(line));
+  return stringLines.map(line => theme.color(line));
 }
 
 function doClock(time) {
   const { columns, rows } = termSize();
-  const clockLines = buildClock(time);
+  const theme = columns < defaultTheme.screenSize.lg.columns ? defaultTheme.screenSize.sm : defaultTheme.screenSize.lg;
+  const clockLines = buildClock(time, theme);
 
   // const clockWidth = clockLines[0].length;
-  const clockWidth = 50;
+  const clockWidth = theme.columns;
   const clockLeft = ' '.repeat(Math.max(0, Math.ceil(columns / 2 - clockWidth / 2)));
 
-  const padding = rows / 2 - 3;
+  const padding = rows / 2 - (theme.rows / 2).toFixed();
 
   printEmptyLines(padding);
-  // printEmptyLines(themeConfig.clock.paddingTop);
   clockLines.forEach(line => print(clockLeft + line));
   printEmptyLines(padding);
-  // printEmptyLines(themeConfig.clock.paddingBottom);
 }
 
 function draw(time) {
